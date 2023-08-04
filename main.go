5db53c2683edbaa55615a86e51889b9d211f896a
MrSharma003 <sprashant145.ms@gmail.com>
2023-08-04 20:39:10 +0530

    created basic project directories

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+
+	routes "GO-JWT/routes"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
+
+	router.GET("/api-1", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"success": "Access granted to api-1"})
+	})
+
+	router.GET("/api-2", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"success": "Access granted to api-2"})
+	})
+
+	router.Run(":" + port)
+
+}
